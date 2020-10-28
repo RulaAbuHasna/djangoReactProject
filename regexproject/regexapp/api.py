7b937fd449e6236192f2415bd46c7d8207b60c74
RulaAbuHasna <rulahasna99@gmail.com>
2020-10-28 23:58:46 +0300

    semi final

--- regexproject/regexapp/api.py
+++ regexproject/regexapp/api.py
@@ -1,12 +1,14 @@
 from rest_framework import viewsets, permissions
 from .serializers import QuerySerializer
+from .models import Query
 # Lead ViewSet
 class Queryviewset(viewsets.ModelViewSet):
+    queryset = Query.objects.all()
     permission_classes = [
-        permissions.IsAuthenticated,
+        permissions.AllowAny,
     ]
     serializer_class = QuerySerializer
-    def get_queryset(self):
-        return self.request.user.queries.all()
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
+    # def get_queryset(self):
+    #     return self.request.user.queries.all()
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
